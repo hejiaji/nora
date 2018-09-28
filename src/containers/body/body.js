52d159b21fb9e9efc044816f4274d7265727f5f2
Jeremy He <jeremy.he@safetyculture.io>
2018-09-28 14:12:51 +1000

    add board part

--- src/containers/body/body.js
+++ src/containers/body/body.js
@@ -43,27 +43,52 @@ const Content = styled.div`
   `}
 `;
 
+const Board = styled.div`
+  margin-top: 80px;
+  height: 200px;
+  background-color: rgb(77, 89, 112);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 36px;
+  color: #fff;
+  
+  ${media.phone`
+    font-size: 30px;
+    margin-top: 10px;
+  `}
+`;
+
 const Description = styled.div`
   word-wrap: break-word;
   line-height: 22px;
   flex: 1;
+  margin-right: 10px;
   
   ${media.phone`
     margin-bottom: 10px;
+     margin-right: 0;
   `}
 `;
 
 class Body extends React.Component {
   render() {
     return (
-      <Container>
-        <Story>Our Story</Story>
-        <About>ABOUT</About>
-        <Content>
-          <Description>Nora was born in March of 2015, since that day she had been a important member in our family. She is the most lovely and obedient baby, she is afraid of strangers, normally will take at least 3 days to get acquainted with you.</Description>
-          <img src={profilePic} alt="profile"/>
-        </Content>
-      </Container>
+      <div>
+        <Container>
+          <Story>Our Story</Story>
+          <About>ABOUT</About>
+          <Content>
+            <Description>Nora was born in March of 2015, since that day she had been a important member in our family.
+              She is the most lovely and obedient baby, she is afraid of strangers, normally will take at least 3 days
+              to get acquainted with you.</Description>
+            <img src={profilePic} alt="profile"/>
+          </Content>
+        </Container>
+        <Board>
+          "Nora, Best cat in the world."
+        </Board>
+      </div>
     );
   }
 }
